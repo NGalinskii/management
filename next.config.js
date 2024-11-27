d46910e7ec61adb047720f654e0f4309ab46efbd
n.galinskiy <n.galinskiy@tonplay.io>
2024-11-27 05:49:24 +0300

    try deploy[1]

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  output: 'export',
-
-  basePath: '/nextjs-github-pages',
-
-  images: {
-    unoptimized: true,
-  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
