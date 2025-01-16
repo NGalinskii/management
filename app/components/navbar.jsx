60c7ce2a176e04982f4902aa72c8a351db805e98
n.galinskiy <n.galinskiy@tonplay.io>
2025-01-16 21:32:16 +0300

    fix

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -60,6 +60,7 @@ function Navbar() {
               className="block px-4 py-2 no-underline outline-none hover:no-underline"
               href="#contact"
             >
+              
               <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
                 Оставить заявку
               </div>
